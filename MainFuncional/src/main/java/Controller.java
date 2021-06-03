8f2e1ba3cb4779164b2ab4a3ac89efaab0045db1
kam1xuseless <wtfomgohohoh@gmail.com>
2021-06-03 15:14:20 +0400

    added saving info from additional task.

--- MainFuncional/src/main/java/Controller.java
+++ MainFuncional/src/main/java/Controller.java
@@ -1,5 +1,7 @@
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.VBox;
@@ -9,6 +11,8 @@ import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ResourceBundle;
 
 
@@ -21,7 +25,9 @@ public class Controller implements Initializable {
     public static Label staticTimeLabel;
     public static Label staticCpuLabel;
     public static Label staticMemLabel;
-
+    public Button saveButton;
+    public Label testLabel;
+    public static String logFileName = "log.txt";
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -29,6 +35,44 @@ public class Controller implements Initializable {
         staticMemLabel = memLabel;
         staticTimeLabel = timeLabel;
 
-        //change to labels. semaphores in FM and here is just an if statements.
+        testLabel.setText(cpuLabel.getText());
+
+        try {
+            File fileName = Paths.get(".").toAbsolutePath().getParent().getParent().resolve("saved.txt").toFile();
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            String str;
+            while ((str = reader.readLine()) != null) {
+                logFileName = str;
+            }
+            reader.close();
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        }
+    }
+
+    public void saveButtonAction() {
+        try {
+            File fileName = Paths.get(".").toAbsolutePath().getParent().getParent().resolve("saved.txt").toFile();
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            String str;
+            while ((str = reader.readLine()) != null) {
+                logFileName = str;
+            }
+            reader.close();
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        }
+
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(logFileName, true));
+            writer.append(" ");
+            writer.append("Information from Main Functional:");
+            writer.append(cpuLabel.getText());
+            writer.append(memLabel.getText());
+            writer.append(timeLabel.getText());
+            writer.append("-----------------------------------");
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        }
     }
 }
